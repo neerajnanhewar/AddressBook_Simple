7c438b723a275e8fb0de2816e5b60a44c6ee91ed
neerajnanhewar <67505109+neerajnanhewar@users.noreply.github.com>
2020-08-11 18:36:29 +0530

    Added : Data Store file added

--- AddEntry.java
+++ AddEntry.java
@@ -3,10 +3,18 @@ public class AddEntry extends javax.swing.JFrame {
 	private javax.swing.JLabel jLabel2;
 	private javax.swing.JLabel jLabel3;
 	private javax.swing.JLabel jLabel4;
+	private javax.swing.JLabel jLabel5;
+	private javax.swing.JLabel jLabel6;
+	private javax.swing.JLabel jLabel7;
+
 	private javax.swing.JTextField jTextField1;
 	private javax.swing.JTextField jTextField2;
 	private javax.swing.JTextField jTextField3;
 	private javax.swing.JTextField jTextField4;
+	private javax.swing.JTextField jTextField5;
+	private javax.swing.JTextField jTextField6;
+	private javax.swing.JTextField jTextField7;
+
 	private javax.swing.JButton jButton1;
 	private javax.swing.JButton jButton2;
 	private javax.swing.JPanel jPanel1;
@@ -22,32 +30,44 @@ public class AddEntry extends javax.swing.JFrame {
 
 		setResizable(true);
 
-		jLabel1 = new javax.swing.JLabel("Name: ");
-		jLabel2 = new javax.swing.JLabel("Address: ");
-		jLabel3 = new javax.swing.JLabel("Tel. No.: ");
-		jLabel4 = new javax.swing.JLabel("Email: ");
+		jLabel1 = new javax.swing.JLabel("First Name: ");
+		jLabel2 = new javax.swing.JLabel("Last Name: ");
+		jLabel3 = new javax.swing.JLabel("Address: ");
+		jLabel4 = new javax.swing.JLabel("City: ");
+		jLabel5 = new javax.swing.JLabel("State: ");
+		jLabel6 = new javax.swing.JLabel("Zip: ");
+		jLabel7 = new javax.swing.JLabel("Phone: ");
 
 		jTextField1 = new javax.swing.JTextField(15);
 		jTextField2 = new javax.swing.JTextField(30);
 		jTextField3 = new javax.swing.JTextField(10);
 		jTextField4 = new javax.swing.JTextField(15);
+		jTextField5 = new javax.swing.JTextField(30);
+		jTextField6 = new javax.swing.JTextField(10);
+		jTextField7 = new javax.swing.JTextField(15);
 
 		jButton1 = new javax.swing.JButton("Add");
 		jButton2 = new javax.swing.JButton("Cancel");
 
-		jPanel1 = new javax.swing.JPanel(new java.awt.GridLayout(4, 1, 1, 5));
+		jPanel1 = new javax.swing.JPanel(new java.awt.GridLayout(7, 1, 1, 5));
 
 		jPanel1.add(jLabel1);
 		jPanel1.add(jLabel2);
 		jPanel1.add(jLabel3);
 		jPanel1.add(jLabel4);
+		jPanel1.add(jLabel5);
+		jPanel1.add(jLabel6);
+		jPanel1.add(jLabel7);
 
-		jPanel2 = new javax.swing.JPanel(new java.awt.GridLayout(4, 1));
+		jPanel2 = new javax.swing.JPanel(new java.awt.GridLayout(7, 1));
 
 		jPanel2.add(jTextField1);
 		jPanel2.add(jTextField2);
 		jPanel2.add(jTextField3);
 		jPanel2.add(jTextField4);
+		jPanel2.add(jTextField5);
+		jPanel2.add(jTextField6);
+		jPanel2.add(jTextField7);
 
 		jPanel3 = new javax.swing.JPanel(new java.awt.FlowLayout());
 
@@ -61,7 +81,7 @@ public class AddEntry extends javax.swing.JFrame {
 
 		jButton1.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent e) {
-				AddressBookMain.addEntry(new AddressBook(jTextField1.getText(), jTextField2.getText(), jTextField3.getText(), jTextField4.getText()));
+				AddressBookMain.addEntry(new AddressBook(jTextField1.getText(), jTextField2.getText(), jTextField3.getText(), jTextField4.getText(),jTextField5.getText(),jTextField6.getText(),jTextField7.getText()));
 			}
 		});
 
